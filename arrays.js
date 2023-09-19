dbdc85cb645b93bb937df5a926f6644da8a24608
Adam Bedient <adambedient@gmail.com>
2023-09-19 17:47:55 -0400

    Show some examples printing elements in specific index positions

--- arrays.js
+++ arrays.js
@@ -5,7 +5,8 @@
 // Call a different Array method on each of the 3 arrays you created above. On the line of code above each method call,
 // write (in a JavaScript comment) an explanation in plain English as to what impact calling that method will have on that specific array.
 // Commit your work.
-// [Intentionally open-ended] Demonstrate your understanding of index positions in this file. You can write an explanation, provide some examples with the Arrays you’ve created, or anything else.
+// [Intentionally open-ended] Demonstrate your understanding of index positions in this file.
+// You can write an explanation, provide some examples with the Arrays you’ve created, or anything else.
 // Commit your work.
 
 var fish = ["tuna", "mackerel", "flounder", "carp"];
@@ -20,3 +21,7 @@ temperatures.push(-4);
 
 // print elements of the array votes beginning with element in the second spot and ending with the element in the third spot.
 console.log(votes.slice(1, 3));
+
+// see above for understanding of index positions. As well, let's print the element in the 3rd spot in fish and the 1st spot in temperatures.
+console.log(fish[2]);
+console.log(temperatures[0]);
