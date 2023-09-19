9217cf00cd9dbbde41bfd6855a0a84ab984004fa
Adam Bedient <adambedient@gmail.com>
2023-09-19 17:33:20 -0400

    Declare 3 arrays

---
+++ arrays.js
@@ -0,0 +1,12 @@
+// Declare a variable that stores an Array of at least 4 Strings.
+var fish = ["tuna", "mackerel", "flounder", "carp"]
+// Declare a variable that stores an Array of at least 4 Numbers.
+var temperatures = [0, 32, 74, 90]
+// Declare a variable that stores an Array of at least 4 Booleans.
+var votes = [true, true, false, true]
+// Commit your work.
+// Call a different Array method on each of the 3 arrays you created above. On the line of code above each method call,
+// write (in a JavaScript comment) an explanation in plain English as to what impact calling that method will have on that specific array.
+// Commit your work.
+// [Intentionally open-ended] Demonstrate your understanding of index positions in this file. You can write an explanation, provide some examples with the Arrays you’ve created, or anything else.
+// Commit your work.
